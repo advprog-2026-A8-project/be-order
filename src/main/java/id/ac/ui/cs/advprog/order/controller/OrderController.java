99d47af6f7a3bb02993a175a06c2a55074e5be2c
DerNiks <derrick@ui.ac.id>
2026-03-06 16:48:57 +0700

    refactor: make OrderRequest DTO to fix sonar security issue

--- src/main/java/id/ac/ui/cs/advprog/order/controller/OrderController.java
+++ src/main/java/id/ac/ui/cs/advprog/order/controller/OrderController.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.order.controller;
 
+import id.ac.ui.cs.advprog.order.dto.OrderRequest;
+import id.ac.ui.cs.advprog.order.enums.OrderStatus;
 import id.ac.ui.cs.advprog.order.model.Order;
 import id.ac.ui.cs.advprog.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +20,14 @@ public class OrderController {
 
     @PostMapping("/checkout")
     public ResponseEntity<Order> checkout(@RequestBody Order orderRequest) {
-        Order savedOrder = orderService.createOrder(orderRequest);
+        Order order = new Order();
+        order.setProductId(orderRequest.getProductId());
+        order.setUserId(orderRequest.getUserId());
+        order.setJumlah(orderRequest.getJumlah());
+        order.setAlamatPengiriman(orderRequest.getAlamatPengiriman());
+        order.setStatus(OrderStatus.PENDING);
+
+        Order savedOrder = orderService.createOrder(order);
         return ResponseEntity.ok(savedOrder);
     }
 
