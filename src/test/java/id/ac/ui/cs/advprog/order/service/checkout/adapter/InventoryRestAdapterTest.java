75ff110ceced78b732d6dfff0050741007bf63d8
DerNiks <derrick@ui.ac.id>
2026-05-04 11:19:20 +0700

    chore: remove unused import

--- src/test/java/id/ac/ui/cs/advprog/order/service/checkout/adapter/InventoryRestAdapterTest.java
+++ src/test/java/id/ac/ui/cs/advprog/order/service/checkout/adapter/InventoryRestAdapterTest.java
@@ -19,7 +19,6 @@ import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
